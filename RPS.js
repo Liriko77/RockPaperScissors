98e27dc6d279f9fad167b1460f747e4c81d26933
Diego Vargas <llgante77@gmail.com>
2025-12-18 06:38:28 -0500

    execute playRound

--- RPS.js
+++ RPS.js
@@ -1,5 +1,10 @@
 console.log("Hello World")
 
+let humanScore = 0;
+let computerScore = 0;
+console.log("Human Score: " + humanScore);
+console.log("Computer Score: " + computerScore);  
+
 function getComputerChoice() {
 
   let randomNumber = Math.random();
@@ -21,5 +26,54 @@ function getComputerChoice() {
 }
 
 
+function getHumanChoice() {
+ let humanChoice = prompt("Choose wisely: rock, paper or scissors");
+ if (humanChoice) {
+  return humanChoice.toLowerCase();
+}
+  return "rock";
+  
+}
 
-console.log(getComputerChoice());
+function playRound(humanChoice, computerChoice) {
+
+  if (humanChoice === computerChoice) {
+
+    return "It's a tie!";
+
+  } else if (
+    (humanChoice === "rock" && computerChoice === "scissors") ||
+    (humanChoice === "paper" && computerChoice === "rock") ||
+    (humanChoice === "scissors" && computerChoice === "paper")
+  ) {
+
+    humanScore++;
+    return "You win! " + humanChoice + " beats " + computerChoice;
+
+  } else {
+
+    computerScore++;
+    return "You lose! " + computerChoice + " beats " + humanChoice;
+
+  }
+
+
+}
+
+const humanSelection = getHumanChoice();
+const computerSelection = getComputerChoice();
+
+console.log("--- Round Start ---");
+console.log("You chose: " + humanSelection);
+console.log("Computer chose: " + computerSelection); 
+
+const result = playRound(humanSelection, computerSelection);
+console.log(result);
+
+console.log("Human Score: " + humanScore);
+console.log("Computer Score: " + computerScore);  
+console.log("--- Round End ---");
+
+
+
+
