a5957e12e58da2d098d2547ce7a1e9095776b286
Diego Vargas <llgante77@gmail.com>
2026-01-06 06:41:07 -0500

    set the stage and get computer choice

--- RPS.js
+++ RPS.js
@@ -10,37 +10,23 @@ function getComputerChoice() {
   let randomNumber = Math.random();
 
   if (randomNumber <= 0.33) {
-
     return "rock";
-
   } else if (randomNumber <= 0.66) {
-
-    return "paper";
-    
+    return "paper";  
   } else {
-
     return "scissors";
-
   }
-
 }
-
 
 function getHumanChoice() {
  let humanChoice = prompt("Choose wisely: rock, paper or scissors");
- if (humanChoice) {
-  return humanChoice.toLowerCase();
-}
-  return "rock";
-  
+  return humanChoice ? humanChoice.toLowerCase() : "rock";
+
 }
 
 function playRound(humanChoice, computerChoice) {
-
   if (humanChoice === computerChoice) {
-
     return "It's a tie!";
-
   } else if (
     (humanChoice === "rock" && computerChoice === "scissors") ||
     (humanChoice === "paper" && computerChoice === "rock") ||
@@ -57,23 +43,36 @@ function playRound(humanChoice, computerChoice) {
 
   }
 
-
 }
 
+function playGame() {
+  for (let round = 1; round <= 5; round++) {
+    console.log("=== Round " + round + " ===");
+    
 const humanSelection = getHumanChoice();
 const computerSelection = getComputerChoice();
 
-console.log("--- Round Start ---");
 console.log("You chose: " + humanSelection);
 console.log("Computer chose: " + computerSelection); 
 
 const result = playRound(humanSelection, computerSelection);
 console.log(result);
 
-console.log("Human Score: " + humanScore);
-console.log("Computer Score: " + computerScore);  
-console.log("--- Round End ---");
+console.log("Scores -> Human: " + humanScore + " | Computer: " + computerScore);
+
+console.log("-------------");
+  }
+
+  console.log("=== Final Score ===");
+console.log(" Human: " + humanScore + " | Computer: " + computerScore);
 
 
-
-
+if (humanScore > computerScore) {
+  console.log("You did it! You won the game!");
+} else if (computerScore > humanScore) {
+  console.log("Hahaha! The AI won the game!");
+} else {
+  console.log("The game ended in a tie!");
+}
+}
+playGame();
